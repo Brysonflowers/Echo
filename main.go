561dadb435fd727c85533de03ffc71c55c9497c5
Brysonflowers <101665972+Brysonflowers@users.noreply.github.com>
2026-08-01 12:20:43 -0500

    Just set up the the server side json to the front end, haveing fun :)

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type Pagedata struct {
+	Junk string `json:"junk"`
+}
+
+func main() {
+	http.HandleFunc("/", main_page)
+
+	fmt.Printf("hi\n")
+
+	http.ListenAndServe(":8900", nil)
+}
+
+func main_page(the_writer http.ResponseWriter, the_request *http.Request) {
+
+	the_data := Pagedata{
+		Junk: "dude that is so cool",
+	}
+
+	renderfunction(the_writer, "main.html", the_data)
+
+}
+
+func renderfunction(the_writer http.ResponseWriter, thefilename string, the_data any) {
+	template_master, the_error := template.ParseFiles("templates/" + thefilename)
+
+	if the_error != nil {
+		http.Error(the_writer, the_error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	a_error := template_master.Execute(the_writer, the_data)
+
+	if a_error != nil {
+		http.Error(the_writer, the_error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
